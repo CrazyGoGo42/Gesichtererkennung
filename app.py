c07348358d3a60bcc63baab6003dd0dd3f583db8
lola <kjhgfd@gmail.com>
2026-02-26 11:46:12 +0100

    Add binary image processing and save binary data

--- app.py
+++ app.py
@@ -21,3 +21,18 @@ for i in range(30):
 np.save("data.npy", face_databank)
 
 face_databank = np.load("data.npy")
+
+
+binary_face_databank = np.zeros((30, 512, 512), dtype=np.uint8)
+
+for i in range(30):
+    _, binary_img = cv2.threshold(face_databank[i], 127, 255, cv2.THRESH_BINARY)
+    binary_face_databank[i] = binary_img
+
+np.save("binary_data.npy", binary_face_databank)
+
+# for i in range(30):                                             Zeigt die Bilder
+#     cv2.imshow(f"Binary Face {i+1}", binary_face_databank[i])
+#     cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
