c7b643f2de8aaf742b8aed0c8a91fb35263bf121
CrazyGoGo42 <github.justifier173@passmail.net>
2026-02-27 19:56:57 +0100

    256 images with new code

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 import os
 
-face_databank = np.zeros((30, 512, 512), dtype=np.uint8)
+face_databank = np.zeros((30, 256, 256), dtype=np.uint8)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-bilder_dir = os.path.join(script_dir, "Bilder")
+bilder_dir = os.path.join(script_dir, "Bilder_256")
 
 for i in range(30):
     img_path = os.path.join(bilder_dir, f"{i+1}.jpg")
@@ -15,7 +15,7 @@ for i in range(30):
         print(f"Konnte die Bilder nicht laden: {img_path}")
         continue
 
-    img = cv2.resize(img, (512, 512))
+    img = cv2.resize(img, (256, 256))
     face_databank[i] = img
 
 np.save("data.npy", face_databank)
@@ -23,7 +23,7 @@ np.save("data.npy", face_databank)
 face_databank = np.load("data.npy")
 
 
-binary_face_databank = np.zeros((30, 512, 512), dtype=np.uint8)
+binary_face_databank = np.zeros((30, 256, 256), dtype=np.uint8)
 
 for i in range(30):
     _, binary_img = cv2.threshold(face_databank[i], 127, 255, cv2.THRESH_BINARY)
